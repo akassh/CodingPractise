2bad96e0a56a455e5b9011332b9d12bef16ff944
akassh.mishra <akassh.mishra@gmail.com>
2015-04-10 00:37:31 -0400

    Added null pointer check

--- CodingPractice/src/com/practice/challenge/ArraysUtility.java
+++ CodingPractice/src/com/practice/challenge/ArraysUtility.java
@@ -2,6 +2,8 @@ package com.practice.challenge;
 
 public class ArraysUtility {
 	public static void printArray(int[] a) {
+		if(a==null||a.length==0)
+			return;
 		for (int i : a)
 			System.out.print( i + " ");
 		System.out.println();
