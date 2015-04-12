e1ccf1cd4a582db917030fcc9da282ab11469fc7
akassh.mishra <akassh.mishra@gmail.com>
2015-04-12 01:08:24 -0400

    Magic Square

---
+++ CodingPractice/src/com/practice/challenge/MagicSquare.java
@@ -0,0 +1,25 @@
+/**
+ * @author Akassh
+ */
+package com.practice.challenge;
+
+public class MagicSquare {
+	public static int[][] generateMagicSquare(int n) {
+		int[][] magicSquare = new int[n][n];
+		for(int c=1,i=n-1,j=n/2;c<=n*n;++c) {
+			magicSquare[i][j] = c;
+			if(magicSquare[(i+1)%n][(j+1)%n]==0) {
+				i = (i+1)%n;
+				j = (j+1)%n;
+			} 
+			else 
+				i = (i-1) < 0 ? n-1 : i-1;
+		}
+		return magicSquare;
+	}
+	
+	public static void main(String[] args) {
+		int n = 5;
+		ArraysUtility.printMatrixTab(MagicSquare.generateMagicSquare(n));
+	}
+}
